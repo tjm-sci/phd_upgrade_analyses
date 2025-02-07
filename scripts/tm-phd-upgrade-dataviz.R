322b4c39845d2a0c897aa4363d150501804498ca
Tom Murphy <t.murphy13@gmail.com>
2025-02-07 14:16:37 +0000

    added more data wrangling

--- scripts/tm-phd-upgrade-dataviz.R
+++ scripts/tm-phd-upgrade-dataviz.R
@@ -7,7 +7,7 @@
 
 path <- file.path("home", "tmurphy", "phd_work",
                   "25-02-06-graphs-for-phd-upgrade")
-setwd(path)
+
 
 # load necessary packages
 install.packages("pacman")
@@ -47,7 +47,7 @@ df2_viab <- `23-05-09-BD_with_viability_dye`
 df3_cd11b <- `23-05-18-cd11b-macs`
 df4_acsa2 <- `23-08-11-ACSA2-MACS-test`
 df5_mcfc <- `23-08-17-MACSvsFACS_data`
-df6_mm24 <- `23-08-17-MACSvsFACS_data`
+df6_mm24 <- `23-09-21-multimacs`
 df7_fans <- `pilot-nuclei-experiments`
 
 # remove the old dfs
@@ -130,5 +130,107 @@ df4_acsa2 <- df4_acsa2 %>%
     )
   )
 
-# dataset 5: df5: 
+# dataset 5: df5:
+# columns names are awful, rename the lot.
+df5_mcfc <- df5_mcfc %>%
+  rename(cells_pct = `Cells | Freq. of Parent`) %>% 
+  rename(cells_count = `Cells | Count`) %>% 
+  rename(cells_vaible_count = `Cells/Viable cells | Count`) %>% 
+  rename(cells_viable_pct = `Cells/Viable cells | Freq. of Cells`) %>% 
+  rename(oligodendrocyte_count = `Cells/Viable cells/ O4+,CD11B- | Count`) %>% 
+  rename(oligodendrocyte_pct = `Cells/Viable cells/ O4+,CD11B- | Freq. of Viable cells`) %>% 
+  rename(microglia_count = `Cells/Viable cells/CD11B+ | Count`) %>% 
+  rename(microglia_pct = `Cells/Viable cells/CD11B+ | Freq. of Viable cells`) %>% 
+  rename(astrocyte_count = `Cells/Viable cells/CD11B-, O4-/ACSA2+ | Count`) %>% 
+  rename(astrocyte_pct = `Cells/Viable cells/CD11B-, O4-/ACSA2+ | Freq. of Viable cells`) %>% 
+  rename(neurons_hi_count = `Cells/Viable cells/CD11B-, O4-/ACSA2-/neurons* | Count`) %>% 
+  rename(neurons_hi_pct = `Cells/Viable cells/CD11B-, O4-/ACSA2-/neurons* | Freq. of Viable cells`) %>% 
+  rename(neurons_lo_count = `Cells/Viable cells/CD11B-, O4-/ACSA2-/neurons** | Count`) %>% 
+  rename(neurons_lo_pct = `Cells/Viable cells/CD11B-, O4-/ACSA2-/neurons** | Freq. of Viable cells`)
 
+# tidy up the names of the FACS samples to broadly match the other sample names
+df5_mcfc <- df5_mcfc %>%
+  mutate(
+    sample = case_match(
+      sample,
+      "2023-08-17_TM gentleMACS brain sort_Sort 1508 A.fcs" ~ "FACS_sort_1508_A.fcs",
+      "2023-08-17_TM gentleMACS brain sort_Sort 1508 B.fcs" ~ "FACS_sort_1508_B.fcs",
+      "2023-08-17_TM gentleMACS brain sort_Sort 1608 B.fcs" ~ "FACS_sort_1608_B.fcs",
+      "2023-08-17_TM gentleMACS brain sort_Sort 1608 C.fcs" ~ "FACS_sort_1608_C.fcs",
+      "2023-08-17_TM gentleMACS brain sort_Sort 1608 D.fcs" ~ "FACS_sort_1608_D.fcs",
+      .default = sample
+    )
+  )
+
+# dataset 6: df6_mm24
+# very similar to df5 except one less column.
+df6_mm24 <- df6_mm24 %>% 
+  rename(cells_count = `Cells | Count`) %>% 
+  rename(cells_vaible_count = `Cells/Viable cells | Count`) %>% 
+  rename(cells_viable_pct = `Cells/Viable cells | Freq. of Cells`) %>% 
+  rename(oligodendrocyte_count = `Cells/Viable cells/ O4+,CD11B- | Count`) %>% 
+  rename(oligodendrocyte_pct = `Cells/Viable cells/ O4+,CD11B- | Freq. of Viable cells`) %>% 
+  rename(microglia_count = `Cells/Viable cells/CD11B+ | Count`) %>% 
+  rename(microglia_pct = `Cells/Viable cells/CD11B+ | Freq. of Viable cells`) %>% 
+  rename(astrocyte_count = `Cells/Viable cells/CD11B-, O4-/ACSA2+ | Count`) %>% 
+  rename(astrocyte_pct = `Cells/Viable cells/CD11B-, O4-/ACSA2+ | Freq. of Viable cells`) %>% 
+  rename(neurons_hi_count = `Cells/Viable cells/CD11B-, O4-/ACSA2-/neurons* | Count`) %>% 
+  rename(neurons_hi_pct = `Cells/Viable cells/CD11B-, O4-/ACSA2-/neurons* | Freq. of Viable cells`) %>% 
+  rename(neurons_lo_count = `Cells/Viable cells/CD11B-, O4-/ACSA2-/neurons** | Count`) %>% 
+  rename(neurons_lo_pct = `Cells/Viable cells/CD11B-, O4-/ACSA2-/neurons** | Freq. of Viable cells`)
+  
+
+# dataset 7:
+# sort out column names
+df7_fans <- df7_fans %>% 
+  rename(nuclei_pct_total = `scatter nuclei | Freq. of Parent`) %>% 
+  rename(single_DAPI_pos_pct_total = `scatter nuclei/single DAPI+ nuclei | Freq. of Total`) %>% 
+  rename(microglial_nuclei_pct_dapi = `scatter nuclei/single DAPI+ nuclei/NeuN-, PU.1+/triple -ve PU.1+ | Freq. of single DAPI+ nuclei`) %>% 
+  rename(microglial_nuclei_mfi = `scatter nuclei/single DAPI+ nuclei/NeuN-, PU.1+/triple -ve PU.1+ | Median (PU1 PE (YG)-A)`) %>% 
+  rename(astrocyte_nuclei_pct_dapi = `scatter nuclei/single DAPI+ nuclei/PU.1-/NeuN-, SOX2+/triple -ve SOX2+ | Freq. of single DAPI+ nuclei`) %>% 
+  rename(astrocyte_nuclei_mfi = `scatter nuclei/single DAPI+ nuclei/PU.1-/NeuN-, SOX2+/triple -ve SOX2+ | Median (SOX2 PE-Cy7 (YG)-A)`) %>% 
+  rename(neuron_nuclei_pct_dapi = `scatter nuclei/single DAPI+ nuclei/PU.1-/SOX2-/triple -ve NeuN+ | Freq. of single DAPI+ nuclei`) %>% 
+  rename(neuron_nuclei_mfi = `scatter nuclei/single DAPI+ nuclei/PU.1-/SOX2-/triple -ve NeuN+ | Median (NeuN AF488*-A)`) %>% 
+  rename(oligo_nuclei_pct_dapi = `scatter nuclei/single DAPI+ nuclei/PU.1-/SOX2-/triple -ve SOX10+ | Freq. of single DAPI+ nuclei`) %>% 
+  rename(oligo_nuclei_mfi = `scatter nuclei/single DAPI+ nuclei/PU.1-/SOX2-/triple -ve SOX10+ | Median (SOX10 AF594*-A)`)
+
+# add column to categorise samples by "pilot 1", "pilot 2", or the 3rd RML brain
+df7_fans <- df7_fans %>% 
+  mutate(
+    experiment = case_when(
+      str_detect(sample, "383") ~ "pilot_1",
+      str_detect(sample, "pilot") ~ "pilot_2",
+      str_detect(sample, "rml") ~ "single_rml"
+    )
+  )
+
+# all of the dataframes contain percentage symbols after the values and the 
+# values are incorectly codes. 
+
+rm_percent_sign <- function(df) {
+  df %>% 
+    mutate(across(everything(), ~ {
+      # leave numeric columns alone
+      if (is.numeric(.)) {
+        .
+      } else {
+        # this is overkill but ensures we operate on character values
+        col_as_char <- as.character(.)
+        if (any(grepl("%", col_as_char, fixed = TRUE), na.rm = TRUE)) {
+          # convert to number and remove %
+          parse_number(col_as_char)
+        } else {
+          .
+        }
+        
+      }
+    }))
+}
+
+# We already removed percentages from the first df so do this on 2-6
+df2_viab  <- rm_percent_sign(df2_viab)
+df3_cd11b <- rm_percent_sign(df3_cd11b)
+df4_acsa2 <- rm_percent_sign(df4_acsa2)
+df5_mcfc  <- rm_percent_sign(df5_mcfc)
+df6_mm24  <- rm_percent_sign(df6_mm24)
+df7_fans  <- rm_percent_sign(df7_fans)
